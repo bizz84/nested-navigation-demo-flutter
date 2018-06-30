927c25ffa2e70cd8d85cb2c0472408ec01005a62
Andrea Bizzotto <bizz84@gmail.com>
2018-06-30 09:52:52 +0100

    Working nested navigation

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:nested_navigation_demo_flutter/bottom_navigation.dart';
 
+
+class DetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: Container(
+        color: Theme.of(context).primaryColor,
+      ),
+    );
+  }
+}
+
+class TabNavigators {
+
+  final redNavigatorKey = GlobalKey<NavigatorState>();
+  final greenNavigatorKey = GlobalKey<NavigatorState>();
+  final blueNavigatorKey = GlobalKey<NavigatorState>();
+
+  GlobalKey<NavigatorState> navigator({TabItem tabItem}) {
+    switch (tabItem) {
+      case TabItem.red:
+        return redNavigatorKey;
+      case TabItem.green:
+        return greenNavigatorKey;
+      case TabItem.blue:
+        return blueNavigatorKey;
+    }
+    return null;
+  }
+}
+
 class App extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => AppState();
 }
 
 class AppState extends State<App> {
+
   TabItem currentTab = TabItem.red;
+  TabNavigators navigators = TabNavigators();
 
   _selectTab(TabItem tabItem) {
     setState(() {
       currentTab = tabItem;
     });
+  }
+
+  _push() {
+    NavigatorState navigatorState = navigators.navigator(tabItem: currentTab).currentState;
+    navigatorState.push(MaterialPageRoute<void>(
+      builder: (BuildContext context) {
+        return DetailPage();
+      },
+    ));
   }
 
   @override
@@ -27,15 +73,29 @@ class AppState extends State<App> {
   }
 
   Widget _buildBody() {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          TabHelper.description(currentTab),
-        ),
-        backgroundColor: TabHelper.color(currentTab),
+    return MaterialApp(
+      navigatorKey: navigators.navigator(tabItem: currentTab),
+      theme: ThemeData(
+        primarySwatch: TabHelper.color(currentTab),
       ),
-      body: Container(
-        color: TabHelper.color(currentTab),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            TabHelper.description(currentTab),
+          ),
+          backgroundColor: TabHelper.color(currentTab),
+        ),
+        body: Container(
+          color: TabHelper.color(currentTab),
+          alignment: Alignment.center,
+          child: FlatButton(
+            child: Text(
+              'PUSH',
+              style: TextStyle(fontSize: 32.0, color: Colors.white),
+            ),
+            onPressed: _push,
+          ),
+        ),
       ),
     );
   }
