97a0a89d3182a1543c1be31f52c68665de97ce8d
Andrea Bizzotto <bizz84@gmail.com>
2018-07-05 21:10:51 +0100

    Update TabNavigator to use MaterialPageRoute

--- lib/tab_navigator.dart
+++ lib/tab_navigator.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:nested_navigation_demo_flutter/bottom_navigation.dart';
 import 'package:nested_navigation_demo_flutter/color_detail_page.dart';
@@ -18,41 +17,40 @@ class TabNavigator extends StatelessWidget {
     var routeBuilders = _routeBuilders(context, materialIndex: materialIndex);
 
     Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) => routeBuilders[TabNavigatorRoutes.detail](context)
-        )
+      context,
+      MaterialPageRoute(
+        builder: (context) => routeBuilders[TabNavigatorRoutes.detail](context),
+      ),
     );
   }
 
-  Map<String, WidgetBuilder> _routeBuilders(BuildContext context, {int materialIndex: 500}) {
+  Map<String, WidgetBuilder> _routeBuilders(BuildContext context,
+      {int materialIndex: 500}) {
     return {
       TabNavigatorRoutes.root: (context) => ColorsListPage(
-        color: TabHelper.color(tabItem),
-        title: TabHelper.description(tabItem),
-        onPush: (materialIndex) => _push(context, materialIndex: materialIndex),
-      ),
+            color: TabHelper.color(tabItem),
+            title: TabHelper.description(tabItem),
+            onPush: (materialIndex) =>
+                _push(context, materialIndex: materialIndex),
+          ),
       TabNavigatorRoutes.detail: (context) => ColorDetailPage(
-        color: TabHelper.color(tabItem),
-        title: TabHelper.description(tabItem),
-        materialIndex: materialIndex,
-      ),
+            color: TabHelper.color(tabItem),
+            title: TabHelper.description(tabItem),
+            materialIndex: materialIndex,
+          ),
     };
   }
 
   @override
   Widget build(BuildContext context) {
-
     var routeBuilders = _routeBuilders(context);
 
     return Navigator(
         key: navigatorKey,
         initialRoute: TabNavigatorRoutes.root,
         onGenerateRoute: (routeSettings) {
-          return PageRouteBuilder(
-              pageBuilder: (context, animation, secondaryAnimation) {
-                return routeBuilders[routeSettings.name](context);
-              }
+          return MaterialPageRoute(
+            builder: (context) => routeBuilders[routeSettings.name](context),
           );
         });
   }
