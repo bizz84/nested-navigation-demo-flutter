6b43986e07d09d6329318f9788e0c2d6a2e36ba2
Andrea Bizzotto <bizz84@gmail.com>
2021-07-05 16:53:57 +0100

    Update packages and fix analysis warnings prior to migration

--- lib/tab_navigator.dart
+++ lib/tab_navigator.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:nested_navigation_demo_flutter/bottom_navigation.dart';
 import 'package:nested_navigation_demo_flutter/color_detail_page.dart';
 import 'package:nested_navigation_demo_flutter/colors_list_page.dart';
 import 'package:nested_navigation_demo_flutter/tab_item.dart';
